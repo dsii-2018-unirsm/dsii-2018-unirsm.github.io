4e2e8d5f570cab15fbd94208a711849f1b8dac73
frmurgia <32902502+frmurgia@users.noreply.github.com>
2018-04-08 15:00:07 +0200

    Add files via upload

--- frmurgia/10PRINT/sketch.js
+++ frmurgia/10PRINT/sketch.js
@@ -1,40 +1,96 @@
 
 /*
-<><><><><><><><><><><><><><><><><>><><>><><>
-.-------------------------------------------.
+<><><><><><><><><><><><><><><><><>
+----------------------------------
 
+generatore di glitch in P5js
 by frmurgia © 2017-18 MIT License
 DSII2018 Lab @UNIRSM
+
+https://it.wikipedia.org/wiki/Glitch_(musica)
 
 10 print
 
 ispirato al lavoro audio/video minimalista
 di Ryoji Ikeda
 
-.------------------------------------------.
-<><><><><><><><><><><><><><><><>><><>><><><>
+----------------------------------
+<><><><><><><><><><><><><><><><><>
 */
 
 
 /*
 ::TO DO::
-
+diminuire la frequenza del random S/H controllandola con il BPM
 
 */
 
+var monoSynth;
+var osc; // creo un oscillatore
+var playing = false; // condizione di play inizialmente falsa
+var button;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
- frameRate(20);
+button=createButton("play");
+
+createCanvas(windowWidth, windowHeight);
+
+  monoSynth=new p5.MonoSynth();
+  osc = new p5.Noise(); // tipo di oscillatore che genera un segnale audio
+                        // composto da tutte le frequenze a stessa potenza
+  osc.setType('pink');
+
+ }
+
+function draw(){
+  background(0);
+  //frameRate(60);
+
+  // verifica la condizione di playing per attivare o meno l'oscillatore
+  if (playing==true){
+      sample_hold();
+   visual();//
+  }
+else {osc.stop();}
 }
 
-function draw() {
- background(0);
- fill(255);
- noStroke();
- translate((windowWidth/2)-50,(windowHeight/2)-100);
- rectMode(CENTER);
+function visual(){
+  fill(255);
+  noStroke();
+  translate((windowWidth/2)-50,(windowHeight/2)-100);
+  rectMode(CENTER);
 
-rect(0, random(200), 100, random(100));
-rect(100, random(200), 100, random(100));
+ rect(0, random(200), 100, random(100));
+ rect(100, random(200), 100, random(100));
 
+
+}
+
+function keyPressed(){ // freccia a destra== audio ON; freccia a sinistra== audio OFF
+    if (keyCode === RIGHT_ARROW){
+        playing=true;
+    }
+    else if (keyCode === LEFT_ARROW){
+        playing=false;
+    }
+
+  }
+
+// funzione sample & hold https://it.wikipedia.org/wiki/Sample_and_hold
+// genera una sequenza random molto veloce di numeri 0 e 1, che corrispondono
+// ad un acceso spento dell'oscillatore.
+function sample_hold() {
+
+var trigger=int(random(2)); //
+  if (trigger==0){
+    osc.start();
+    var midiVal = round( random(50,72) );
+    monoSynth.triggerAttack(midiVal, random(2) );
+
+  }
+  else{
+      monoSynth.triggerRelease();
+  osc.stop();
+
+  }
 }
