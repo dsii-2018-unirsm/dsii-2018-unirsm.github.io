8cfb3bec3e3adf3a50d08a1588292d41810be301
Flatman <36697402+erMaijettaro@users.noreply.github.com>
2018-05-22 15:23:53 +0200

    Update sketch.js

--- erMaijettaro/Il Cubo Della Situazione/Prototipi/data detector/sketch.js
+++ erMaijettaro/Il Cubo Della Situazione/Prototipi/data detector/sketch.js
@@ -6,34 +6,31 @@
 
 
 
-//var data1;
+var data1;
 var data2;
 var data3;
 
 
+var fonte='ansa'
 
-
-//news1= 'arte'
-var news2= 'calcio'
+var news1= 'arte'
+var news2= 'calco'
 var news3= 'siria'
 
+var lucedata1, lucedata2, lucedata3;
+var titdata1, titdata2, titdata3;
+var oradata1, oradata2, oradata3;
+var descdata1, descdata2, descdata3;
 
-
+var lucedata1_new, lucedata1_old;
 
 function preload() {
 
+  var url1 = 'https://newsapi.org/v2/everything?q='+news1+'&sources='+fonte+'&apiKey=2a45165c1efc458eb2bb251e241f0b51';
+  var url2 = 'https://newsapi.org/v2/everything?q='+news2+'&sources='+fonte+'&apiKey=2a45165c1efc458eb2bb251e241f0b51';
+  var url3 = 'https://newsapi.org/v2/everything?q='+news3+'&sources='+fonte+'&apiKey=2a45165c1efc458eb2bb251e241f0b51';
 
-
-
-  //var url1 = 'https://newsapi.org/v2/everything?q='+news1+'&sources=ansa&apiKey=2a45165c1efc458eb2bb251e241f0b51';
-  var url2 = 'https://newsapi.org/v2/everything?q='+news2+'&sources=ansa&apiKey=2a45165c1efc458eb2bb251e241f0b51';
-  var url3 = 'https://newsapi.org/v2/everything?q='+news3+'&sources=ansa&apiKey=2a45165c1efc458eb2bb251e241f0b51';
-
-
-
-
-
-  //data1 = loadJSON(url1);
+  data1 = loadJSON(url1);
   data2 = loadJSON(url2);
   data3 = loadJSON(url3);
 }
@@ -48,7 +45,24 @@ function setup() {
   createCanvas(displayWidth, displayHeight);
   background(100);
 
+
+   lucedata1 = data1.totalResults
+   titdata1 = data1.articles[0].title
+  oradata1 = data1.articles[0].publishedAt
+    descdata1 = data1.articles[0].description
+
+    lucedata2 = data2.totalResults
+    titdata2 = data2.articles[0].title
+    oradata2 = data2.articles[0].publishedAt
+    descdata2 = data2.articles[0].description
+
+    lucedata3 = data3.totalResults
+    titdata3 = data3.articles[0].title
+    oradata3 = data3.articles[0].publishedAt
+    descdata3 = data3.articles[0].description
+
 }
+
 
 
 
@@ -59,67 +73,42 @@ background(200);
 
 
 
-
-/*
-  var lucedata1 = data1.totalResults
-  var titdata1 = data1.articles[0].title
-  var oradata1 = data1.articles[0].publishedAt
-  var descdata1 = data1.articles[0].description
-*/
-
-  var lucedata2 = data2.totalResults
-  var titdata2 = data2.articles[0].title
-  var oradata2 = data2.articles[0].publishedAt
-  var descdata2 = data2.articles[0].description
-
-
-
-  var lucedata3 = data3.totalResults
-  var titdata3 = data3.articles[0].title
-  var oradata3 = data3.articles[0].publishedAt
-  var descdata3 = data3.articles[0].description
-
-
-
-
-
   textAlign(CENTER);
   //text(descdata, 0, height - 30, width, 30);
+var a= 300;
+var b=800;
+var c=1200;
 
 
-/*
-text ('pubblicato il: '+ oradata1, 350,200);
-text ('pubblicato il: '+ descdata1, 350,300);
-text ('sottotitolo: '+ titdata1, 350,250);
-text ('il numero delle notizie trovate è: '+ lucedata1, 350,350);
-*/
+  text ('pubblicato il: '+ oradata1,  a,200);
+  text ('pubblicato il: '+ descdata1, a,300);
+  text ('sottotitolo: '+ titdata1, a,250);
+  text ('il numero delle notizie trovate è: '+ lucedata1, a,350);
 
-text ('pubblicato il: '+ oradata2, 350,200);
-text ('sottotitolo: '+ descdata2, 350,300);
-text ('titolo: '+ titdata2, 350,250);
-text ('il numero delle notizie trovate è: '+ lucedata2, 350,350);
+  text ('pubblicato il: '+ oradata2, b,200);
+  text ('sottotitolo: '+ descdata2, b,300);
+  text ('titolo: '+ titdata2, b,250);
+  text ('il numero delle notizie trovate è: '+ lucedata2, b,350);
 
-
-
-text ('pubblicato il: '+ oradata3, 850,200);
-text ('sottotitolo: '+ descdata3, 850,300);
-text ('titolo: '+ titdata3, 850,250);
-text ('il numero delle notizie trovate è: '+ lucedata3, 850,350);
+  text ('pubblicato il: '+ oradata3, c,200);
+  text ('sottotitolo: '+ descdata3, c,300);
+  text ('titolo: '+ titdata3, c,250);
+  text ('il numero delle notizie trovate è: '+ lucedata3, c,350);
 
 
-fill(0);
-rect (250,400, 200, 200);
-rect (750,400, 200, 200);
+  fill(0);
+  rect (a-100,400, 200, 200);
+  rect (b-100,400, 200, 200);
+  rect (c-100,400, 200, 200);
 
 
-fill(255);
-//rect (250,400, 200, lucedata1/10);
-rect (250,400, 200, lucedata2/10);
-rect (750,400, 200, lucedata3/10 );
+  fill(255);
+  rect (a-100,600, 200, -lucedata1/10); // map(x, 0, 1000, 0, 100)
+  rect (b-100,600, 200, -lucedata2/10);
+  rect (c-100,600, 200, -lucedata3/10 );
 
 
 
 
 
 }
-
