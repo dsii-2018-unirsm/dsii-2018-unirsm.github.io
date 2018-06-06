d2c39400109dbd7caa15df5315d24607c091b2a2
claudia <36658968+claudiapnf@users.noreply.github.com>
2018-06-06 15:54:03 +0200

    Delete mv_01.js

--- claudiapnf/making_visible/alpha/mv_01/mv_01.js
+++
@@ -1,8 +0,0 @@
-const googleTrends = require('google-trends-api');
-googleTrends.relatedQueries({ keyword: ['design'],startTime: new Date(Date.now() - (1 * 60 * 60 * 1000)), granularTimeResolution: true, timezone: 20})
-.then(function(results){
-  console.log(results);
-})
-.catch(function(err){
-  console.error('Oh no there was an error', err);
-});
