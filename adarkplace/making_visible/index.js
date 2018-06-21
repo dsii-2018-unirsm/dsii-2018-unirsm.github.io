5609d56a1954ea9ae3de3df09fee2f3cdd91d177
Federico Cortesi <31067595+adarkplace@users.noreply.github.com>
2018-06-21 20:31:25 +0200

    Add files via upload

--- adarkplace/making_visible/index.js
+++ adarkplace/making_visible/index.js
@@ -1,11 +1,11 @@
+// Federico Cortesi @adarkplace © 2018 MIT License
+// JOURNEY - PROTO MAP | Ravenna, IT
+// Educational purpose, made for DSII2018 lab @UniRSM
 
-//------------------------------CREAZIONE MAPPA DA IMG O URL
+//------------------------------CREAZIONE MAPPA DA IMG
 
 var bounds = [[-1200,-1200], [1200,1200]];
 var imageURL = 'img/antarctica_3-02.jpg';
-// var imageURL1 = 'img/map_base.jpg';
-// var imageURL2 = 'img/map_labels.png';
-// var imageURLonline = 'https://eoimages.gsfc.nasa.gov/images/imagerecords/78000/78592/antarctica_etm_2000001_lrg.jpg';
 
 var map = L.map('mapid', {
   crs: L.CRS.Simple,
@@ -18,8 +18,6 @@ var map = L.map('mapid', {
 });
 
 var image = L.imageOverlay(imageURL, bounds).addTo(map);
-// var image1 = L.imageOverlay(imageURL1, bounds).addTo(map);
-// var image2 = L.imageOverlay(imageURL2, bounds).addTo(map);
 
 //------------------------------MARKER CUSTOM
 
@@ -37,10 +35,8 @@ var poleicon = L.icon({
           iconSize: [90 , 90],
 });
 
-//------------------------------LETTURA SPREADSHEET E VARIABILI
+//------------------------------ARRAY POSIZIONI MARKER E INDICE
 
-// var url = "https://spreadsheets.google.com/feeds/list/1JDSbPNu-fHFAWMpIPzLiX_GG77CxRFYXaYVpT2_Fc-A/od6/public/values?alt=json";
-// var markers = [];
 var markers2 = [
             [-534, -190, "<p><b>Framheim</b></p>78.37.60.S<br>163.40.0.O"],
             [-473, -170, "<p><b>Depot 1</b></p>80.00.00.S<br>163.40.00.O"],
@@ -62,17 +58,8 @@ var markers2 = [
           ];
 var indice = 1;
 
-//------------------------------PRELOAD & SETUP
-
-// function preload() {
-//   loadJSON(url, gotSpreadsheet);
-// }
-// //
-// //
-// function setup() {
-// }
-
 //------------------------------POPUP
+
 var popupcontent1 = "<img src='img/marker1.jpg' width='139px'/>";
 var popupcontent2 = "<img src='img/marker2.jpg' width='139px'/>";
 var popupcontent3 = "<img src='img/marker3.jpg' width='139px'/>";
@@ -92,7 +79,7 @@ var popupcontent16 = "<img src='img/marker16.jpg' width='139px'/>";
 var popupcontent17 = "<img src='img/marker17.jpg' width='139px'/>";
 
 
-var popup0 = L.popup({maxWidth: 100, closeButton: false, autoClose: true, closeOnClick:true}).setLatLng([markers2[0][0],markers2[0][1]]).setContent(markers2[0][2]);
+var popup0 = L.popup({maxWidth: 100, closeButton: false, autoClose: true, closeOnClick:true}).setLatLng([markers2[0][0],markers2[0][1]]);
 var popup1 = L.popup({maxWidth: 100, closeButton: false, autoClose: true, closeOnClick:true}).setLatLng([markers2[1][0]-12,markers2[1][1]+30]).setContent(popupcontent1);
 var popup2 = L.popup({maxWidth: 100, closeButton: false, autoClose: true, closeOnClick:true}).setLatLng([markers2[2][0]-12,markers2[2][1]+30]).setContent(popupcontent2);
 var popup3 = L.popup({maxWidth: 100, closeButton: false, autoClose: true, closeOnClick:true}).setLatLng([markers2[3][0]-12,markers2[3][1]+30]).setContent(popupcontent3);
@@ -110,76 +97,7 @@ var popup14 = L.popup({maxWidth: 100, closeButton: false, autoClose: true, close
 var popup15 = L.popup({maxWidth: 100, closeButton: false, autoClose: true, closeOnClick:true}).setLatLng([markers2[15][0]-12,markers2[15][1]+30]).setContent(popupcontent15);
 var popup16 = L.popup({maxWidth: 100, closeButton: false, autoClose: true, closeOnClick:true}).setLatLng([markers2[16][0]-12,markers2[16][1]+30]).setContent(popupcontent16);
 
-// var popupMain = L.popup({ closeButton: false, autoClose: true, closeOnClick:true}).setLatLng([-564, -270]).setContent('<p><b>Welcome to Journey</b></p> Discover the map<br>and try to find all the checkpoint<br>like a true pioneer');
-
-// var popupWelcome = new L.FeatureGroup();
-// popupWelcome.addLayer(popupMain);
-// map.addLayer(popupMain);
-// map.on('zoomend', function() {
-//     if (map.getZoom() <2){
-//             map.removeLayer(popupMain);
-//     }
-// });
-
-//------------------------------LETTURA SPREADSHEET
-
-// function gotSpreadsheet(journey) {
-//   for (var i = 0; i < journey.feed.entry.length; i++) {
-//     var ma = {
-//                   "XXX": journey.feed.entry[i].gsx$x.$t,
-//                   "YYY": journey.feed.entry[i].gsx$y.$t,
-//                   "Data": journey.feed.entry[i].gsx$data.$t,
-//                   "Nome": journey.feed.entry[i].gsx$nome.$t,
-//                   // "latitude": journey.feed.entry[i].gsx$latitude.$t
-//                   // "longitude": journey.feed.entry[i].gsx$longitude.$t
-//                   "barometer8am": journey.feed.entry[i].gsx$b8am.$t,
-//                   "barometer2pm": journey.feed.entry[i].gsx$b2pm.$t
-//
-//               }
-//     ma["XXX"] = int(ma["XXX"]);
-//     ma["YYY"] = int(ma["YYY"]);
-//     ma["barometer8am"] = int(ma["barometer8am"]);
-//     ma["barometer2pm"] = int(ma["barometer2pm"]);
-//     markers.push(ma);
-//   }
-// }
-
-//------------------------------DISEGNO MARKER DA ARRAY SPREADSHEET
-
-// L.marker([markers2[0][0], markers2[0][1]], {icon: boaticon}).bindTooltip(markers2[0][2]).addTo(map).on('click', nextmarker);
-//
-// function nextmarker() {
-//   var lat = markers[indice]["XXX"];
-//   var lon = markers[indice]["YYY"];
-//   var popupText = markers[indice]["Nome"];
-//   L.marker([lat, lon], {icon: campicon,}).bindTooltip(popupText, {direction: 'right'}).addTo(map).on('click', nextmarker);
-//   indice = indice + 1;
-// }
-
-//------------------------------DISEGNO MARKER DA ARRAY MANUALE------OK-------
-
-// L.marker([markers2[0][0], markers2[0][1]], {icon: boaticon}).bindTooltip(markers2[0][2]).addTo(map).on('click', nextmarker);
-//
-// function nextmarker() {
-//   var lon = markers2[indice][1];
-//   var lat = markers2[indice][0];
-//   var popupText = markers2[indice][2];
-//   L.marker([lat, lon], {icon: campicon,}).bindTooltip(popupText, {direction: 'right'}).addTo(map).on('click', nextmarker);
-//   indice = indice + 1;
-//   if (indice > 2){
-//     map.addLayer(popup2);
-//   }
-//   console.log (indice);
-// }
-
 //------------------------------DISEGNO MARKER DA ARRAY MANUALE
-
-
-// for (var i = 1; i < markers2.legth; i++){
-//   var tempLat = markers2[i][0];
-//   var tempLong = markers2[i][1];
-//   L.marker([tempLat, tempLong], {icon: campicon}).addTo(map).on('mouseover', nextmarker[i]).on('mouseout', nextmarkerclose[i]);
-// }
 
 L.marker([markers2[0][0], markers2[0][1]], {icon: boaticon}).addTo(map).on('mouseover', nextmarker0).on('mouseout', nextmarkerclose0);
 
@@ -200,7 +118,7 @@ L.marker([markers2[14][0], markers2[14][1]], {icon: campicon,}).addTo(map).on('m
 L.marker([markers2[15][0], markers2[15][1]], {icon: campicon,}).addTo(map).on('mouseover', nextmarker15).on('mouseout', nextmarkerclose15);
 L.marker([markers2[16][0], markers2[16][1]], {icon: poleicon,}).addTo(map).on('mouseover', nextmarker16).on('mouseout', nextmarkerclose16);
 
-//------------------------------TIMELINE
+//------------------------------TIMELINE A DESTRA
 
 document.getElementById('navigation').onclick = function(e) {
     e.preventDefault();
@@ -272,50 +190,6 @@ function nextmarkerclose15() {map.removeLayer(popup15);}
 function nextmarker16() {map.addLayer(popup16);}
 function nextmarkerclose16() {map.removeLayer(popup16);}
 
-// function nextmarker2() {
-//   L.marker([markers2[2][0], markers2[2][1]], {icon: campicon,}).bindTooltip(markers2[2][2], {direction: 'right'}).addTo(map).on('click', nextmarker3);
-//   map.addLayer(popup2);
-// }
-//
-// function nextmarker3() {
-//   L.marker([markers2[3][0], markers2[3][1]], {icon: campicon,}).bindTooltip(markers2[3][2], {direction: 'right'}).addTo(map).on('click', nextmarker4);
-//   map.removeLayer(popup2);
-//   map.addLayer(popup3);
-// }
-//
-// function nextmarker4() {
-//   L.marker([markers2[4][0], markers2[4][1]], {icon: campicon,}).bindTooltip(markers2[4][2], {direction: 'right'}).addTo(map).on('click', nextmarker5);
-//   map.removeLayer(popup3);
-// }
-
-//------------------------------CREAZIONE IMMAGINI OVERLAY
-
-// var imageUrl = 'img/marker_camp.png',
-// imageBounds = [[-500, -160], [-490, -150]];
-// L.imageOverlay(imageUrl, imageBounds).addTo(map);
-
-
-//------------------------------AGGIUNTA DI UN ELEMENTO GRAFICO STATICO SU PAGINA
-
-// L.Control.Watermark = L.Control.extend({
-//     onAdd: function(map) {
-//         var img = L.DomUtil.create('img');
-//         img.src = 'img/marker_camp.png';
-//         img.style.width = '50px';
-//         return img;
-//     },
-//
-//     onRemove: function(map) {
-//         // Nothing to do here
-//     }
-// });
-//
-// L.control.watermark = function(opts) {
-//     return new L.Control.Watermark(opts);
-// }
-//
-// L.control.watermark({ position: 'topright' }).addTo(map);
-
 //------------------------------POPUP DI DEBUG PER COORDINATE
 
 // var popup = L.popup();
@@ -328,7 +202,7 @@ function nextmarkerclose16() {map.removeLayer(popup16);}
 // }
 // map.on('click', onMapClick);
 
-//------------------------------LINEA CON MOUSE OVER E POPUP
+//------------------------------LINEA TRATTEGGIATA DEL PERCORSO
 
 L.polyline([
 [markers2[0][0], markers2[0][1]],
@@ -349,7 +223,3 @@ L.polyline([
 [markers2[15][0], markers2[15][1]],
 [markers2[16][0], markers2[16][1]]
 ],{ weight: 2, color: 'black', opacity: 0.4,dashArray: '4,8',lineJoin: 'round'}).addTo(map);
-
-//------------------------------AREA CON MOUSE OVER E POPUP
-
-// L.polygon([[-450, 126], [-388, 161], [-308, 115], [-284, 73], [-204, -18], [-266, -74]],{color: '#fe57a1', stroke:0}).bindTooltip('Look revealing label!').addTo(map)
