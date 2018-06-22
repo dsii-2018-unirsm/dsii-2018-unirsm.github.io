5682dd7bb9f76e382f974fc74bb1a77a1f43d625
frmurgia <32902502+frmurgia@users.noreply.github.com>
2018-06-22 23:36:57 +0200

    Add files via upload

--- frmurgia/Random word/Random Text v1/Sketch.js
+++ frmurgia/Random word/Random Text v1/Sketch.js
@@ -4,7 +4,6 @@
 ----------------------------------
 
 Generatore random di testo in P5js
-v2
 by frmurgia © 2017-18 MIT License
 DSII2018 Lab @UNIRSM
 
@@ -12,74 +11,29 @@ DSII2018 Lab @UNIRSM
 <><><><><><><><><><><><><><><><><>
 */
 
-
  var parole;
+  var ind;
  // creo una funzione in cui precarico il file con tutte le parole presenti nel dizionario inglese
  function preload() {
    parole = loadStrings('js/test.txt');
  }
-
-function estrai(){
-   var ind = floor(random(parole.length));
-   return ind;
-}
+ function random_word(){
+   // genero un numero casuale intero tra 0 e la dimensione massima dell'array
+     // in cui sono contenute tutte le parole
+   var caso=floor(random(parole.length));// estraggo casualmente dall'array parole un numero casuale attraverso l'indice
+   return caso;
+ }
 
  function setup() {
-
-   var ind = floor(random(parole.length)); // genero un numero casuale intero tra 0 e la dimensione massima dell'array
-                                          // in cui sono contenute tutte le parole
-   text(parole[ind], 10, 10, 80, 80);     // estraggo casualmente dall'array parole un numero casuale attraverso l'indice
+   createCanvas(windowWidth, windowHeight);
+   ind= random_word();
  }
- 'use strict';
 
- var x = 0;
- var y = 0;
- var stepSize = 5.0;
-
- var font = 'IBM Plex Mono';
- var fontSizeMin = 3;
- var angleDistortion = 0.0;
-
- var counter = 0;
-
- function setup() {
-   createCanvas(displayWidth, displayHeight);
-   background(255);
-   cursor(CROSS);
-
-   x = mouseX;
-   y = mouseY;
-
-   textFont(font);
-   textAlign(LEFT);
-   fill(0, 102);
- }
 
  function draw() {
-console.log("x: "+x);
-console.log("y: "+y);
-   if (mouseIsPressed && mouseButton == LEFT) {
-     var d = dist(x, y, mouseX, mouseY);
-     textSize(fontSizeMin + d / 2);
-     var newLetter =" "+parole[estrai()].charAt(counter);
-
-
-     stepSize = textWidth(newLetter)
-
-     if (d > stepSize) {
-       var angle = atan2(mouseY - y, mouseX - x);
-
-       push();
-       translate(x, y);
-       rotate(angle + random(angleDistortion));
-       text(newLetter, 10, 0);
-       pop();
-
-       counter++;
-       if (counter >= parole[estrai()].length) counter = 0;
-
-       x = x + cos(angle) * stepSize;
-       y = y + sin(angle) * stepSize;
-     }
-   }
+ background(255);
+ textFont('IBM Plex Mono');
+   textSize(16);
+   text('la parola estratta è: '+parole[ind],windowWidth/2-150, windowHeight/2);     // prendo dall'array in cui sono contenute tutte le PAROLE
+                                          // la parola nella posizione estratta casualmente
  }
