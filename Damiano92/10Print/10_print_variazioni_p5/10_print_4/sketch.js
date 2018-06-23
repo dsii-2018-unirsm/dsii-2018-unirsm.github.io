ece495b58afed514f4d19faa978a2bc553c14258
Damiano92 <36682835+Damiano92@users.noreply.github.com>
2018-06-23 20:30:39 +0200

    Update sketch.js

--- Damiano92/10Print/10_print_variazioni_p5/10_print_4/sketch.js
+++ Damiano92/10Print/10_print_variazioni_p5/10_print_4/sketch.js
@@ -1,29 +1,34 @@
+
+
 var c = 0;
 
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(displayWidth, displayHeight);
   wave = new p5.Oscillator();
   wave.setType('triangle');
   wave.amp(3);
-  wave.freq(15);
+  wave.freq(10);
 
 }
 
 function draw() {
   background(195, 44, 60);
-  if (mouseIsPressed) {
 
-    for (var y = 0; y < height; y += 25) {
+  for (var y = 0; y < height; y += 15+mouseX/3) {
 
-      for (var x = 0; x < width; x += 25) {
-        wave.start();
-        var c = int(random(2)) * 25;
-        strokeWeight(2);
-        line(x + c, y, x + 25 - c, y + 25);
-
-      }
+    for (var x = 0; x < width; x += 15+mouseY/2) {
+      wave.start();
+      var c = int(random(2)) * 25;
+      strokeWeight(3);
+      stroke(0);
+      point(x + c, y, x + 25 - c, y + 25);
+      stroke(255);
+      point(x + c, y, x + 25 - c, y + 25);
+      
     }
+    //rotate(mouseX);
+    
   }
 }
