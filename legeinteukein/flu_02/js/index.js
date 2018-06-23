26b53972a9dce96a9fa58e67a2ae2dc3d381e519
legeinteukein <36818789+legeinteukein@users.noreply.github.com>
2018-06-23 11:09:14 +0200

    Add files via upload

--- legeinteukein/flu_02/js/index.js
+++ legeinteukein/flu_02/js/index.js
@@ -1,10 +1,9 @@
-
-// FLU by legeinteukein [Speranzavita, decessiinfluenza]
+// FLU by legeinteukein [SPERANZAVITA, decessiinfluenza]
 // 2018 © legeinteukein, Daniele @Fupete and the course DSII2018 @UniRSM
 // github.com/fupete — github.com/dsii-2018-unirsm
 // Educational purposes, MIT License, 2018, San Marino
-// P5js gdoc example inspired on Gist https://gist.github.com/claytical/6a929f14964c867e07d8 by @claytical
 
+// P5js gdoc example inspired on Gist https://gist.github.com/claytical/6a929f14964c867e07d8 by @claytical
 
 var url = "https://spreadsheets.google.com/feeds/list/1dYj_UHjd_WXFqHBNzIAWvk8UhDTEQ-RuzU_cFr2fxBc/1/public/values?alt=json";
 //var url = "https://spreadsheets.google.com/feeds/list/2PACX-1vQXBXKP9_a0jsq_GpOjSyjP1gI2LexMCMBWoqvaKynNJVd2mrsQZbas7ClWFBJawcCk6MKNh-by8OGN/od6/public/values?alt=json";
@@ -13,7 +12,8 @@ var url = "https://spreadsheets.google.com/feeds/list/1dYj_UHjd_WXFqHBNzIAWvk8Uh
 var ogg = []; // < array di oggetti/classi
 var grid = 0;
 var ruota = true;
-
+var button;
+var valueClick = 1;
 
 
 
@@ -23,29 +23,76 @@ function setup() {
   loadJSON(url, gotSpreadsheet, 'jsonp');   // richiedi i dati formato JSON e poi chiama la funzione gotSpreadsheet
 
   rectMode(CORNER);
-  textFont("Raleway");
-
+  textFont("Roboto");
 
 } // setup()
+
+
+
 function draw() {
-// piccolo loop per verificare di avere i dati, stampa su schermo cerchi con i colori presenti nel google doc
+
+
+
+  // piccolo loop per verificare di avere i dati, stampa su schermo cerchi con i colori presenti nel google doc
   grid = width/(ogg.length+1);
   background(0,0,10);
+  var co = color(0, 100, 80);
 
-  text("2015", 200, 70);
-  text("2010", 400, 70);
-  text("2005", 600, 70);
+//  text("OBJECTS : " + ogg.length, 10,20); // < stampa il numero oggetti in alto a sx
+
+
+textFont("Helvetica");
+textSize(50);
+fill (255);
+textAlign(CENTER);
+text("2015", 400, 90);
+fill (0,0,40);
+text("2010", 800, 90);
+text("2005", 1200, 90);
+
+
+textAlign(LEFT);
+  textFont("Helvetica");
+  textSize(15);
+  fill (255);
+  textStyle (NORMAL);
+  text("fascia d'età espressa in numero", 40, 160);
+
+
+  fill (255, 24, 10);
+  textStyle (NORMAL);
+  text("numero di decessi per influenza", 40, 200);
+
+
+  fill (25, 204, 100);
+  textStyle (NORMAL);
+  text("anni di speranza di vita", 40, 180);
+
 
 
   for (var i=0; i<ogg.length; i++) {   // (muovi e) mostra tutti gli oggetti
-     ogg[i].mostra();
+   // ogg[i].muovi();,
+    ogg[i].mostra();
    }
+
+
+
+
+
+
 
 } // draw()
 
 
-function gotSpreadsheet(datiEsterni) {
 
+
+
+
+
+
+
+
+function gotSpreadsheet(datiEsterni) {
   console.log(datiEsterni.feed.entry.length); // < debug, numero righe della tabella
   for (var i = 0; i < datiEsterni.feed.entry.length; i++) {
     // costruzione dell'oggetto singolo, la riga
@@ -73,97 +120,72 @@ function gotSpreadsheet(datiEsterni) {
               }
 
 
+             //printLn(c);
 
+
+    console.log(c); // < debug, verifica oggetto 1x1
     // e ora generiamo un nuovo oggetto classe "Oggetto"
-
-  ogg.push(new Oggetto(i, c.anni2015, c.speranzadivita2015, c.morti2015, c.anni2010, c.speranzadivita2010, c.morti2010, c.anni2005, c.speranzadivita2005, c.morti2005,));
-  //console.log(ogg);
+  ogg.push(new Oggetto(i, c.anni2015, c.speranzadivita2015, c.morti2015,));
+  // ogg.push(c);
   }
-}
+} // gotSpreadsheet(colori)
 // DEFINIZIONE DELLA CLASSE OGGETTI "Oggetto"
 
 
-function Oggetto(_id, _anni, _speranzav, _morti, _anni10, _speranzav10, _morti10, _anni05, _speranzav05, _morti05) {
+
+
+
+function Oggetto(_id, _anni, _speranzav, _morti,/* _saturation, _brightness, _morti, _forma*/) {
   // DATI E COSTRUTTORE
-  var oid;
-  var oanni;
-  var osperanzav;
-  var omorti;
+  this.id = Number(_id); // < Number() converte in numero intero la stringa
+  this.anni2015 = _anni;
+  // < Number() converte in numero intero la stringa
+  this.speranzadivita2015 = _speranzav;
+  this.morti2015 =_morti;
 
-  if (mouseX >=390 && mouseX<=440 && mouseY >=50 && mouseY<=100) {
-  var oid = _id;
-  var oanni = _anni10;
-  var osperanzav = _speranzav10;
-  var omorti = _morti10;
-
- this.id = Number(oid);
-  this.anni2010 = oanni;
-  this.speranzadivita2010 = osperanzav;
-  this.morti2010 =omorti;
+  this.mostra = function() {
 
 
-} if (mouseX >=590 && mouseX<=640 && mouseY >=50 && mouseY<=100) {
-  var oid = _id;
-  var oanni = _anni05;
-  var osperanzav = _speranzav05;
-  var omorti = _morti05;
-
-  this.id = Number(oid);
-  this.anni2005 = oanni;
-  this.speranzadivita2005 = osperanzav;
-  this.morti2005 =omorti;
 
 
-} else{
-  var oid = _id;
-  var oanni = _anni;
-  var osperanzav = _speranzav;
-  var omorti = _morti;
-
-  this.id = Number(oid);
-  this.anni2015 = oanni;
-  this.speranzadivita2015 = osperanzav;
-  this.morti2015 =omorti;
-
-}
-
-this.mostra = function() {
-  push();
 
     noStroke();
     fill(255);
+    textFont("Helvetica");
+    textSize(12);
     textAlign(LEFT, CENTER);
     push();
-    translate(grid + (oid * grid),height/3);
+    translate(grid + (this.id * grid),height/3);
 
 
-    text(oanni,-40,10,);
+    text(this.anni2015,-40,10,);
 
     rotate(PI/2);
-
-    text(omorti,30,00);
-    text(osperanzav,30,30);
-
-
-    // disegna barrette
-    rotate(3*PI/2);
+    fill(255, 24, 10);
+    text(this.morti2015,30,00);
     fill(25, 204, 100);
-    rect(-38, 60, 10, osperanzav);
+    text(this.speranzadivita2015,30,30);
+
+// disegna barrette
+    rotate(3*PI/2);
+   fill(25, 204, 100);
+    rect(-38, 60, 10, this.speranzadivita2015);
 
 
     fill(255, 24, 10);
-    rect(-8, 60, 10, omorti);
+    rect(-8, 60, 10, this.morti2015);
 
     pop();
 
   }
-
-
-
-
 }
 
+// prova funzione per cambiiare dati per anno
+//NON FUNZIONA
 
+
+
+//function mousePressed() {}
 
 
 // se ridimensiona la finestra ricalcola width e height canvas
