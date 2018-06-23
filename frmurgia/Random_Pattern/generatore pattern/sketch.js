7fbeb27274fd34bcfdaeff64b11065286eafc2af
frmurgia <32902502+frmurgia@users.noreply.github.com>
2018-06-23 10:47:05 +0200

    Add files via upload

--- frmurgia/Random_Pattern/generatore pattern/sketch.js
+++ frmurgia/Random_Pattern/generatore pattern/sketch.js
@@ -1,24 +1,39 @@
 
+/*
+<><><><><><><><><><><><><><><><><>
+----------------------------------
+
+example in P5js
+by frmurgia © 2017-18 MIT License
+DSII2018 Lab @UNIRSM
+
+----------------------------------
+<><><><><><><><><><><><><><><><><>
+*/
+
 var DimCubo = 100; // grandezza dei quadrati
 var countBorder = 3;
 
 var modes = chain;
 var currModeFn = chain;
 var clrs = ['#152A3B', '#158ca7', '#F5C03E', '#D63826', '#F5F5EB'];
-
-
+var numero;
+var numero_1;
+var variabile=4;
 function setup() {
-  frameRate(1);
+  frameRate(2);
 var finestra= createCanvas(windowWidth, windowHeight);
-  //rectMode(CENTER);
+  rectMode(CENTER);
   noStroke();
 }
 
 function draw() {
-  background(255);
+
+  numero_1=(variabile%random(500));
+   background(255);
   for (var y = DimCubo / 2; y < height; y+=DimCubo) {
     for (var x = DimCubo / 2; x < width; x+=DimCubo) {
-      queueNum = shuffleArray([ 0, 1, 2, 3, 4 ]);
+      queueNum = shuffleArray([ 0, 1, 2, 3, 4 ]); //Estraggo un valore casuale dall'array dei colori
       fill(clrs[queueNum[0]]);
       rect(x, y, DimCubo, DimCubo);
       push();
@@ -34,14 +49,14 @@ function draw() {
 }
 
 function chain(x, y, clrs) {
-  rotate(radians(90 * Math.round(random(1, 5))));
+  rotate(radians(90 * Math.round(numero_1)));// sostituire
   fill(clrs[queueNum[1]]);
   arc(x - DimCubo / 2, y, DimCubo, DimCubo, radians(270), radians(450));
     noStroke();
   fill(clrs[queueNum[2]]);
   arc(x + DimCubo / 2, y, DimCubo, DimCubo, radians(90),  radians(270));
     noStroke();
-  rotate(radians(90 * Math.round(random(1, 5))));
+  rotate(radians(90 * Math.round(numero_1)));// sostituire
   fill(clrs[queueNum[1]]);
   arc(x, y + DimCubo / 2, DimCubo, DimCubo, radians(180), radians(360));
     noStroke();
@@ -55,15 +70,11 @@ function shuffleArray(array) {
   var j, temp;
   for (var i = array.length - 1; i > 0; i--) {
     j = Math.floor(Math.random() * (i + 1));
+
     temp = array[i];
     array[i] = array[j];
     array[j] = temp;
   }
 
   return array;
-}
-function mousePressed() {
-saveFrames('out', 'png', 1, 25, function(data) {
-  print(data);
-});
 }
